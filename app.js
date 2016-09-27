7d782b968068d02c27485b67f2b6f8c96e639077
Clint Madariaga <clintmad21@hotmail.com>
2016-09-27 16:26:11 -0600

    Added css

--- app.js
+++ app.js
@@ -12,18 +12,39 @@ function drawSongs(songList) {
     for (var i = 0; i < songList.length; i++) {
         var song = songList[i]
         template += `<div id="song">
-    <div class ="text-container">
-      <h2>${song.title}</h2>
-      <img src="${song.albumArt}" alt=""/>
-      <p>${song.artist}</h2>
-      <p>${song.collection}</p>
-      <p>${song.price}</p>
-      <audio controls>
-        <source src="${song.preview}" type = ""/>
-      </audio>  
+    <div class ="text-container container-fluid">
+        <div class="row card-white">            
+            <div class="col-xs-12 col-md-2">
+                <img class="art" src="${song.albumArt}" alt=""/>
+            </div>
+            <div class="col-xs-12 col-md-6">
+                <h3>${song.title}</h3>
+                <p>${song.artist}</h2>
+                <p>${song.collection}</p>
+            </div>
+            <div class="col-xs-12 col-md-3">
+                <audio controls>
+                    <source src="${song.preview}" type = ""/>
+                </audio>
+            </div>
+            <div class="col-xs-12 col-md-1">
+                <p>${song.price}</p>
+            </div>
+        </div>
     </div>
   </div>`
 
         songElement.innerHTML = template
     }
 }
+
+// function pauseSong(songList){
+// var newSong = document.getElementById('song')
+// var currentSong = song.preview;
+// if(currentSong && currentSong != newSong){
+//     currentSong.pause();
+// }
+// if(currentSong.paused){
+//     newSong.play();
+// }
+// }
