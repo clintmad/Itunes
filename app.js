ed4f48cda44c8bbdbfd1f565427c2ccd470b27f4
Clint Madariaga <clintmad21@hotmail.com>
2016-09-28 13:02:40 -0600

    Added more CSS, changed the navbar

--- app.js
+++ app.js
@@ -11,24 +11,24 @@ function drawSongs(songList) {
     var songElement = document.getElementById('song')
     for (var i = 0; i < songList.length; i++) {
         var song = songList[i]
-        template += `<div id="song">
+        template += `<div id="song" class="songText">
     <div class ="text-container container-fluid">
         <div class="row card-white">            
             <div class="col-xs-12 col-md-2">
-                <img class="art" src="${song.albumArt}" alt=""/>
+                <img class="art img-thumbnail" src="${song.albumArt}" alt=""/>
             </div>
             <div class="col-xs-12 col-md-6">
                 <h3>${song.title}</h3>
-                <p>${song.artist}</h2>
-                <p>${song.collection}</p>
+                <p>By: ${song.artist}</h2>
+                <p>Album: ${song.collection}</p>
             </div>
-            <div class="col-xs-12 col-md-3">
+            <div class="col-xs-12 col-md-3">                
                 <audio controls>
                     <source src="${song.preview}" type = ""/>
                 </audio>
             </div>
             <div class="col-xs-12 col-md-1">
-                <p>${song.price}</p>
+                <p><a href="#">$${song.price}</a></p>
             </div>
         </div>
     </div>
