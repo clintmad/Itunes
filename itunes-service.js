bc2942b3a8f62a0285644508e8cae50a71a2c824
Clint Madariaga <clintmad21@hotmail.com>
2016-09-29 09:59:51 -0600

    fix movie search

--- itunes-service.js
+++ itunes-service.js
@@ -2,7 +2,7 @@ var itunes = {
     getMusicByArtist: function(artist, cb) {
       
       var url = '//bcw-getter.herokuapp.com/?url=';
-      var url2 = 'https://itunes.apple.com/search?term=' + artist;
+      var url2 = 'https://itunes.apple.com/search?media=music&term=' + artist;
       var apiUrl = url + encodeURIComponent(url2);
       
       $('#get-music-button').text('LOADING....');
